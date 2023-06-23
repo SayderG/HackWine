6cd7cd04d41ec408f067e7c9b8b16c0c6df051d4
Sayder <zimens05@mail.ru>
2023-06-24 02:21:54 +0300

    redis port open

--- API/redis.py
+++ API/redis.py
@@ -5,7 +5,7 @@ redis = None
 
 async def start_redis():
     global redis
-    redis = await aioredis.from_url("redis://localhost")
+    redis = await aioredis.from_url("redis://localhost:6379")
 
 
 async def stop_redis():
