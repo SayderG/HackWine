df1bafd62d371a05d7f4e416a579c8815ac78e73
Sayder <zimens05@mail.ru>
2023-06-24 02:18:21 +0300

    test ws

--- API/routers/chat.py
+++ API/routers/chat.py
@@ -9,34 +9,104 @@ router = APIRouter()
 html = """
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws/group/user");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
+<head>
+    <title>WebSocket Chat</title>
+    <script type="text/javascript">
+        var socket;
+
+        function connectWebSocket(group, username) {
+            var wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+            var wsUrl = wsProtocol + window.location.host + "/ws/" + group + "/" + username;
+
+            socket = new WebSocket(wsUrl);
+
+            socket.onopen = function () {
+                console.log("WebSocket connection established.");
             };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
+
+            socket.onmessage = function (event) {
+                var message = event.data;
+                // Handle received message
+                console.log("Received message:", message);
+            };
+
+            socket.onclose = function () {
+                console.log("WebSocket connection closed.");
+            };
+        }
+
+        function sendMessage(message) {
+            if (socket.readyState === WebSocket.OPEN) {
+                socket.send(message);
             }
-        </script>
-    </body>
+        }
+
+        function connectToMessageStream(group) {
+            var wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
+            var wsUrl = wsProtocol + window.location.host + "/ws/" + group + "/stream";
+
+            socket = new WebSocket(wsUrl);
+
+            socket.onopen = function () {
+                console.log("WebSocket connection established for message stream.");
+            };
+
+            socket.onmessage = function (event) {
+                var message = event.data;
+                // Handle received message from the message stream
+                console.log("Received message from stream:", message);
+            };
+
+            socket.onclose = function () {
+                console.log("WebSocket connection for message stream closed.");
+            };
+        }
+
+        function disconnectWebSocket() {
+            if (socket) {
+                socket.close();
+            }
+        }
+    </script>
+</head>
+<body>
+    <h1>WebSocket Chat</h1>
+    <div>
+        <h2>Connect to WebSocket</h2>
+        <form onsubmit="return false;">
+            <label for="group">Group:</label>
+            <input type="text" id="group" name="group">
+            <br>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+            <br>
+            <button onclick="connectWebSocket(document.getElementById('group').value, document.getElementById('username').value)">
+                Connect
+            </button>
+        </form>
+    </div>
+    <div>
+        <h2>Send Message</h2>
+        <form onsubmit="return false;">
+            <label for="message">Message:</label>
+            <input type="text" id="message" name="message">
+            <br>
+            <button onclick="sendMessage(document.getElementById('message').value)">Send</button>
+        </form>
+    </div>
+    <div>
+        <h2>Message Stream</h2>
+        <form onsubmit="return false;">
+            <label for="streamGroup">Group:</label>
+            <input type="text" id="streamGroup" name="streamGroup">
+            <br>
+            <button onclick="connectToMessageStream(document.getElementById('streamGroup').value)">
+                Connect to Stream
+            </button>
+        </form>
+    </div>
+    <button onclick="disconnectWebSocket()">Disconnect</button>
+</body>
 </html>
 """
 
